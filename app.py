27ce34703f83f6aff3f6ea24e81f8c9a8d0fe8a1
Kyle Norris <r.kyle.norris@gmail.com>
2025-04-05 18:11:08 -0400

    entry point to run dash

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
+import dash_bootstrap_components as dbc
 from dash import Dash
 from dashboard.layout import serve_layout
 from dashboard.callbacks import register_callbacks
 
-app = Dash(__name__)
+# Use a dark Bootstrap theme (e.g., "CYBORG")
+app = Dash(__name__, external_stylesheets=[dbc.themes.SLATE])
 app.title = "PC Stats Dashboard"
 
 app.layout = serve_layout()
 register_callbacks(app)
 
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    app.run(debug=True)
